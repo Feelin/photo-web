36366aee42ddec5521898f661aef07b2d50923a9
羽陵 <yulin.fyl@alibaba-inc.com>
2015-08-23 17:57:23 +0800

    home page

---
+++ controllers/get.js
@@ -0,0 +1,12 @@
+'use strict';
+var views = require('co-views');
+var parse = require('co-body');
+
+var render = views(__dirname + '/../views', {
+  map: { html: 'swig' }
+});
+
+module.exports.home = function *home() {
+  this.body = yield render('index', {'assetsHost':"http://localhost"});
+};
+
